92a0424bed4a07f243634346b566185ef8660930
Mwani Ngemi <47126505+MwaniN@users.noreply.github.com>
2022-03-27 17:14:02 -0400

    Updating username and email and profile picture
    Users can now update their username and profile picture on the account page after signing up/logging in. Also the form will display the current username/email until they change it. You can also change the profile picture used and upload a new picture that will replace the current one used for the user.

--- flask_project_file/forms.py
+++ flask_project_file/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -37,6 +38,7 @@ class UpdateAccountForm(FlaskForm):
 							validators=[DataRequired(), Length(min = 2, max = 20)])
 	email = StringField('Email', 
 						validators=[DataRequired(), Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
 	submit = SubmitField('Update')
 
 	def validate_username(self, username):
@@ -46,7 +48,7 @@ class UpdateAccountForm(FlaskForm):
 				raise ValidationError('That username is taken. Please choose another one.')
 
 	def validate_email(self, email):
-		if username.email != current_user.email:
+		if email.data != current_user.email:
 			user = User.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError('That email is taken. Please choose another one.')
