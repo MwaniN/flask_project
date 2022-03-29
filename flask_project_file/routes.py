56056032f85540611e5c0550455e7924b81bccb3
Mwani Ngemi <47126505+MwaniN@users.noreply.github.com>
2022-03-28 21:06:58 -0400

    Resize user images when updating the profile pic
    Now we resize any pics larger than 125 x 125 to be 125 x 125 max when users update their profile picture. This will save space in the DB and make the page load faster.

--- flask_project_file/routes.py
+++ flask_project_file/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_project_file import app, db, bcrypt
 from flask_project_file.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -11,14 +12,14 @@ posts = [
 
 	{
 		'author': 'Billy Bob',
-		'title' : '1st Blog Post',
+		'title' : '1st Post',
 		'content' : 'First post content',
 		'date_posted': 'April 20, 2022'
 	}
 	,
 	{
 		'author': 'Jane Austen',
-		'title' : '2nd Blog Post',
+		'title' : '2nd Post',
 		'content' : 'Second post content',
 		'date_posted': 'April 21, 2022'
 	}
@@ -77,7 +78,13 @@ def save_picture(form_picture):
 	_, f_ext = os.path.splitext(form_picture.filename)
 	picture_fn = random_hex + f_ext
 	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-	form_picture.save(picture_path)
+
+	output_size = (125, 125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+
+
+	i.save(picture_path)
 	return picture_fn
 
 
